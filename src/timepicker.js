a6096e4b58fe89145814f4a29e611a7cc9dea17f
Hat52 <tanolihamzaali@gmail.com>
2022-09-06 02:03:16 +0500

    removed unwanted files, Added time selector

---
+++ src/timepicker.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import './app.css'
+const hoursList24 = ["01","02","03","04","05","06","07","08","09",10,11,12,13,14,15,16,17,18,19,20,21,22,23,"00"]
+export default function TimePicker({value,setValue}) {
+    const [hours,setHours] = useState("00")
+    const [min,setMin] = useState("00")
+    // const [value,setValue] = useState()
+    useEffect(()=>{
+        if(value.length) {
+            const splitedValue = value.split(":")
+            setHours(splitedValue[0])
+            setMin(splitedValue[1])
+        }
+    },[value])
+    const onChangeHours = (value) =>{
+        console.log("Here")
+        setHours(value)
+        setValue(`${value}:${min}`)
+    }
+    const onChangeMin = (value) =>{
+        console.log("Here")
+        setMin(value)
+        setValue(`${hours}:${value}`)
+    }
+    return(
+        <div className="d-flex time-picker-container ms-2">
+            <select className="select-one" onChange={({target:{value}})=>onChangeHours(value)} value={`${hours}:`}>
+                {
+                    hoursList24.map(hour=>{
+                        return(
+                            <option value={hour}>{hour}</option>
+                        )
+                    })
+                }
+            </select>
+            <span>:</span>
+            <select className="select-two" onChange={({target:{value}})=>onChangeMin(value)} value={`${min}:`}>
+                <option value={"00"}>00</option>
+                <option value={30}>30</option>
+            </select>
+            <span>:</span>
+            <select className="select-am" onChange={({target:{value}})=>onChangeMin(value)}>
+                <option value={"am"}>AM</option>
+                <option value={"pm"}>PM</option>
+            </select>
+        </div>
+        
+    )
+}
